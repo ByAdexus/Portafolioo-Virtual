2827b22f652ee0eae2064e4a63cd7a355b859333
ByAdexus <sergiotejedarex@gmail.com>
2024-06-11 09:12:39 -0700

    Avance contacto

--- src/View/HomeView.jsx
+++ src/View/HomeView.jsx
@@ -110,25 +110,51 @@ const HomeView = () => {
         </div>
       </div>
     {/* Sección "Contáctame"  https://classtop-15d2c.web.app/ */}
-    <div className="contact-section">
+     {/* Sección "Contáctame" */}
+     <div className="contact-section">
         <div className="contact">
-        <h2><i className="fas fa-project-diagram"></i> Contacto</h2>
+          <h2><i className="fas fa-envelope"></i> Contáctame</h2>
           <div className="hr-line hr-line-contact"></div>
-          <form className="contact-form">
-            <div className="form-group">
-              <label htmlFor="name">Nombre:</label>
-              <input type="text" id="name" name="name" required />
+          <div className="contact-container">
+            <div className="contact-left">
+              <div className="address details">
+                <i className="fas fa-map-marker-alt"></i>
+                <div className="topic">Address</div>
+                <div className="text-one">Surkhet, NP12</div>
+                <div className="text-two">Birendranagar 06</div>
+              </div>
+              <div className="phone details">
+                <i className="fas fa-phone-alt"></i>
+                <div className="topic">Phone</div>
+                <div className="text-one">+0098 9893 5647</div>
+                <div className="text-two">+0096 3434 5678</div>
+              </div>
+              <div className="email details">
+                <i className="fas fa-envelope"></i>
+                <div className="topic">Email</div>
+                <div className="text-one">codinglab@gmail.com</div>
+                <div className="text-two">info.codinglab@gmail.com</div>
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="email">Correo electrónico:</label>
-              <input type="email" id="email" name="email" required />
+            <div className="contact-right">
+              <div className="topic-text">Send us a message</div>
+              <p>If you have any work from me or any types of queries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
+              <form action="#">
+                <div className="input-box">
+                  <input type="text" placeholder="Enter your name" required />
+                </div>
+                <div className="input-box">
+                  <input type="email" placeholder="Enter your email" required />
+                </div>
+                <div className="input-box message-box">
+                  <textarea placeholder="Enter your message" required></textarea>
+                </div>
+                <div className="button">
+                  <input type="submit" value="Send Now" />
+                </div>
+              </form>
             </div>
-            <div className="form-group">
-              <label htmlFor="message">Mensaje:</label>
-              <textarea id="message" name="message" rows="5" required></textarea>
-            </div>
-            <button type="submit" className="btn">Enviar</button> 
-          </form>
+          </div>
         </div>
       </div>
     </div>
